20f033df4a3851d41d5cb07eb5b6c435c39f5102
wesker <alexandr_071@bk.ru>
2020-08-22 17:14:43 +0300

    Updates

---
+++ precache-manifest.a345a026ca75ac144c36fadc73cd7ddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6dd48c11344dc2df2eb8ed781c50df",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "0926404cdb73161e2331",
+    "url": "/cats/static/css/main.0254e14f.chunk.css"
+  },
+  {
+    "revision": "bc5bd00b8f8acb00ba28",
+    "url": "/cats/static/js/2.4f97190a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats/static/js/2.4f97190a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0926404cdb73161e2331",
+    "url": "/cats/static/js/main.8f3d275e.chunk.js"
+  },
+  {
+    "revision": "a9acce6b6ea6cf2c34f1",
+    "url": "/cats/static/js/runtime-main.f66b96ba.js"
+  },
+  {
+    "revision": "3d7e1ad70582e5c2e1e79c70f54739ad",
+    "url": "/cats/static/media/back.3d7e1ad7.png"
+  },
+  {
+    "revision": "f8cb1cae4c4dddae300712deb4783b04",
+    "url": "/cats/static/media/cat.f8cb1cae.png"
+  }
+]);
